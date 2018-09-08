4dde408bb6b1a38b54793cc371352f06c693246b
Iman Jawad <imanj12@gmail.com>
2018-09-08 17:06:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,8 +89,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  holiday_supplies.each do |season, holiday|
   
-
 end
 
 
